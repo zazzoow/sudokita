b40375d9afb91485329e031ae6c2aab95490b7e5
zazzoow <boukli-hamza@hotmail.com>
2020-05-13 00:47:59 +0200

    15

--- js/index.js
+++ js/index.js
@@ -2,60 +2,77 @@ let httpRequest = new XMLHttpRequest();
 let rightArrow = document.getElementById("slider-next");
 let leftArrow = document.getElementById("slider-previous");
 let div = document.getElementById("lol");
+let sans = document.createElement("img");
+div.appendChild(sans);
+// let sans = document.getElementById("sans");
 let i = 0;
 
 ///////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 ///////////////////function////////////////////////
 function carrousel(img,slider) {
-   console.log(i);
-   localStorage.getItem("image",i);
-   switch(slider) {
-       case "next":
-            i++;
-       break;
-       case "previous":
-           i--;
-       break;
-    };
-    if( i == img.length ) {
-      i = 0;
-    }
-    else if ( i < 0 ) {
-      i = img.length - 1;
-    };
-    addImage(img,i) = img[i];
-    console.log(i);
-    localStorage.setItem("image",i);
+    indexChange(slider);
+    addImage(img);
    };
 
 
-function addImage(img,i) {
+function addImage(img) {
+  console.log(i);
+
   let count = 6;
   let j = 0;
-  for( let j = 0 ; j < count + i ; j++) {
-    let images = document.createElement("img");
 
-    images.src = img[i];
+  reboot(count,img);
+  localStorage.getItem("image",i);
+  console.log(i);
+
+  for(j = j + i ; j  + i < count + i ; j++) {
+
+    let images = document.createElement("img");
+    images.src = img[j + i];
     images.width = "100";
-    images.height = "100"; //PAS FINI LA FONCTION EN COURS DE BUILDING
-    console.log(images);
+    images.height = "100";
+
+    // sans.replaceWith(images);
+    // console.log(i);
+    console.log(span.parentNode);
     div.appendChild(images);
-    reboot(i,count,img);
+
   };
+  localStorage.setItem("image",i);
 };
 
-function reboot(i,count,img) {
-  if( i + count = img.length ) {
+function reboot(count,img) {
+  if( i + count > img.length ) {
    i = 0;
+   console.log(i);
+
+   return localStorage.setItem("image",i);
+  }
+  else if ( i < 0 ) {
+    i = img.length - count - 1;
+    console.log(i);
+
+    return localStorage.setItem("image",i);
   }
 };
 
+function indexChange(slider) {
+  console.log(i);
+  localStorage.getItem("image",i);
+  switch(slider) {
+      case "next":
+           i++;
+      break;
+      case "previous":
+          i--;
+      break;
+   };
+};
 ////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////
 //////////////////action////////////////////////
 
-// mul.innerHTML = 'bb' * 5
 
 httpRequest.open("GET","controller/gethintController.php?q=",true);
 httpRequest.send();
@@ -64,8 +81,8 @@ httpRequest.onreadystatechange = function() {
     if(httpRequest.readyState == XMLHttpRequest.DONE) {
       if(httpRequest.status === 200) {
           let img = JSON.parse(this.responseText);
-          console.log(img[1]);
-          addImage(img);//dont forget
+
+          addImage(img);
           rightArrow.addEventListener("click", function() {
               carrousel(img,'next');
           });
--- views/index.views.php
+++ views/index.views.php
@@ -2,11 +2,13 @@
 
    <main>
      <div>
-         <img id ="slider-next" src = "image/noun_Arrow_left.png" alt="right" width = "20%" height = "20% ">
+         <img id ="slider-previous" src = "image/noun_Arrow_left.png" alt="right" width = "20%" height = "20% ">
         <div id ="lol">
-
+          <span id = "span">
+            <img id ="sans" src = "image/sans.jpeg" alt = "sans" width = "20%" height = "30%">
+          </span>
         </div>
-         <img id ="slider-previous" src = "image/noun_Arrow_right.png" alt="left" width = "20%" height = "20% ">
+         <img id ="slider-next" src = "image/noun_Arrow_right.png" alt="left" width = "20%" height = "20% ">
       </div>
    </main>
 
