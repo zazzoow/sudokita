f2abf32e57f2a8438bfed56988bef96e54709a9e
zazzoow <boukli-hamza@hotmail.com>
2020-06-02 13:07:03 +0200

    25

--- views/error404.php
+++ views/error404.php
@@ -1,21 +1,9 @@
+<?php require 'views/head.views.php'; ?>
 
-<!DOCTYPE html>
-<!-- code by webdevtrick ( https://webdevtrick.com ) -->
- 
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>CREATIVE ERROR 404 PAGE - WEBDEVTRICK.COM</title>
-  <script type="text/javascript" src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  
-      <link rel="stylesheet" href="style.css">
-  
-</head>
- 
-<body>
- 
+<main>
+
     <h1> ERROR 404 </h1>
-	
-</body>
-</html>
+
+</main>
+
+<?php require 'views/foot.views.php'; ?>
